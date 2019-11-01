ffed7c43e8874f148c38dc1927494b8e01754e0e
Stanslaus <mstans8@gmail.com>
2019-11-01 08:20:05 +0300

    a look at variables - refresher

--- scripts/vars.js
+++ scripts/vars.js
@@ -1,1 +1,9 @@
-var firstName = 'John';
+var firstName = 'Stanslaus';
+console.log(firstName);
+
+var lastName = 'Omondi';
+var age = 32;
+var job = 'teacher';
+var fullAge  = true;
+
+console.log(firstName,lastName, 'is', age, 'years old and is of full age? ', fullAge, 'and is a', job);
