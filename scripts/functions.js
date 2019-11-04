6620efabf7f34a15e759c1f82abfb29037577dfd
Stanslaus <mstans8@gmail.com>
2019-11-04 22:11:46 +0300

    finish work on the function expressions and function declarations

--- scripts/functions.js
+++ scripts/functions.js
@@ -16,3 +16,44 @@ function yearToRetirement(yearOfBirth,retirementAge){
 }
 
 console.log(yearToRetirement(1994, 65));
+
+
+/****
+*
+**Function Statements and Expressions
+**
+*/
+
+
+//Function Declaration
+function jobDescriptionOne(job,firstName){
+    //function body
+}
+
+
+//Function Expression
+var jobDescriptionTwo = function(job, firstName){
+    //function body
+    switch(job){
+        case 'teacher':{
+            return firstName + ' teaches junior coders how to program';
+        }
+        case 'driver':{
+            return firstName + ' ensures driving school tutorials are up t date!';
+        }
+        case 'designer':{
+            return firstName + ' handles all the interior decor things for us!';
+
+        }
+        case 'coach':{
+            return firstName + ' ensures our physical fitness is up to standards!';
+
+        }
+        default:{
+            return 'Job description not yet defined!';
+        }
+    }
+}
+
+
+console.log(jobDescriptionTwo('designer','James'));
