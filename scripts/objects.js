8cf88b8e55720666164c17db7595b4c9e63ab537
Stanslaus <mstans8@gmail.com>
2019-11-04 23:45:56 +0300

    introduction to objects, properties and methods

---
+++ scripts/objects.js
@@ -0,0 +1,30 @@
+/**
+ * Objects and Properties
+ *
+ *
+ */
+
+
+//Object literal
+var john = {
+    'firstName': 'John',
+    'lastName': 'Smith',
+    'gender': 'male',
+    'dateOfBirth': 1990,
+    'family': ['Jane', 'Mark', 'Emilly'],
+    'job': 'teacher',
+    'isMarried': false
+}
+
+
+console.log(john);
+console.log(john['family']);
+
+
+//new object syntax`
+let jane = new Object();
+jane.surname = 'Omondi';
+jane.firstName = 'Jane';
+jane.lastName = 'Donata';
+
+console.log(jane);
