522c1975e4f8d2c86e8372dd0504e17805f29bb6
Stanslaus <mstans8@gmail.com>
2019-11-04 22:50:56 +0300

    arrays update

---
+++ scripts/arrays.js
@@ -0,0 +1,16 @@
+/***
+*
+* Arrays
+*
+*
+*/
+
+var names = ['John', 'James', 'Mark', 'Jane','Peter'];
+var years = new Array(1990,1987,1988,1972,1986);
+
+console.log(names[3]);
+console.log(names);
+names.push('Stanslaus');
+names.unshift('Beginning');
+console.log(names); 
+
