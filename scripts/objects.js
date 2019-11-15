b9f0f60a77849e2602a35ea3dbc11e5d94482a00
Stanslaus <mstans8@gmail.com>
2019-11-15 05:47:49 +0300

    update the tutorial on the THIS keyword

--- scripts/objects.js
+++ scripts/objects.js
@@ -10,19 +10,22 @@ var john = {
     'firstName': 'John',
     'lastName': 'Smith',
     'gender': 'male',
-    'dateOfBirth': 1990,
+    'yearOfBirth': 1990,
     'family': ['Jane', 'Mark', 'Emilly'],
     'job': 'teacher',
     'isMarried': false,
     'calcAge': function () {
-        return 2019 - this.dateOfBirth;
+        return 2019 - this.yearOfBirth;
+    },
+    'demoThis': function(){
+        console.log(this);
     }
 }
 
-
-console.log(john);
-console.log(john.calcAge());
-console.log(john['family']);
+//
+//console.log(john);
+//console.log(john.calcAge());
+//console.log(john['family']);
 
 
 //new object syntax`
@@ -31,9 +34,23 @@ jane.surname = 'Omondi';
 jane.firstName = 'Jane';
 jane.lastName = 'Donata';
 
-console.log(jane);
+//console.log(jane);
 
 
 /**
  * Objects and Methods
  */
+
+//The THIS keyword
+//console.log(this);
+
+
+calcAge(1994);
+
+function calcAge(year) {
+    console.log(2019 - year);
+    console.log(this);
+}
+
+
+john.demoThis();
